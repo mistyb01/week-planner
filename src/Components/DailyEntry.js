84b4921931e3abdb0e6e686d08e2222a9274b8f9
Misty Becker <mistyb01@uw.edu>
2022-11-20 10:50:20 -0800

    created function to expand/collapse entries

--- src/Components/DailyEntry.js
+++ src/Components/DailyEntry.js
@@ -3,14 +3,19 @@ import React from 'react';
 
 function DailyEntry({date, tasks, notes}) {
   function handleEntryActions(e) {
-    console.log(e);
+    if (e.target.tagName != 'BUTTON') return;
+    let id = e.target.dataset.date;
+    let entryContent = document.getElementById(id);
+    entryContent.hidden = !entryContent.hidden;
+    entryContent.hidden ? e.target.innerText = "v" : e.target.innerText = "^";
   }
+  
   return (
     <div onClick={handleEntryActions} class="daily-entry">
-      <h2>
-        {date}
-        <button data-date={date}>V</button>
-      </h2>
+      <div class="entry-header">
+        <h2>{date}</h2>
+        <button data-date={date}>v</button>
+      </div>
       <div class="entry-content" id={date}>
         <ul>
         {tasks && tasks.map(item => 
@@ -25,14 +30,16 @@ function DailyEntry({date, tasks, notes}) {
             </div>
             )}
         </ul>
-      </div>
-      <div class="input-form">
+
+        <div class="input-form">
         <input type="text" class="new-item-form"></input>
         <select name="type">
           <option value="task">task</option>
           <option value="note">note</option>
         </select>
         <button class="add-content"> + </button>
+      </div>
+
       </div>
     </div>
     
